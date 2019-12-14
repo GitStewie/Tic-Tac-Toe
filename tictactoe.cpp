7a31a389835aa1d0a99b320b6ea33ae2cf1b1cca
GitStewie <ckhosh2@illinois.edu>
2019-12-13 21:53:52 -0600

    Starting Code for Tic Tac Toe game.

---
+++ tictactoe.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Header that allows cout and cin
+
+using namespace std; // Defaults cout and cin to std::cout and std::cin
+
+int main() { // Main function that is automatically called at execution
+  cout << "Holy shit this looks so pretty";
+}
